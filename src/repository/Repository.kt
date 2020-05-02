35717cb382c2cd985c4c85f74b2b3bb764b3b8db
foo7print <foo7print@users.noreply.github.com>
2020-05-02 21:20:22 +0900

    5. Model and Repository

---
+++ src/repository/Repository.kt
@@ -0,0 +1,14 @@
+package com.raywenderlich.repository
+
+import com.raywenderlich.model.*
+
+interface Repository {
+    suspend fun add(phrase: EmojiPhrase): EmojiPhrase
+    suspend fun phrase(id: Int): EmojiPhrase?
+    suspend fun phrase(id: String): EmojiPhrase?
+    suspend fun phrases(): List<EmojiPhrase>
+    suspend fun remove(phrase: EmojiPhrase)
+    suspend fun remove(id: Int): Boolean
+    suspend fun remove(id: String): Boolean
+    suspend fun clear()
+}
